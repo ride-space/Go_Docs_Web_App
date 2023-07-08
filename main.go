ddd423f90a6a307c7c01c1a3d968ae53bc98b835
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2023-07-08 23:23:48 +0900

    textファイルの作成をする記述を追加

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	// "log"
+	// "net/http"
+)
+
+type Page struct {
+	Title string
+	Body []byte
+}
+
+// 0600のパーミッションはウェブサーバーを立ち上げた人が読み書きできる
+
+func (p *Page) save() error {
+filename := p.Title + ".txt"
+return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil,err
+	}
+	return &Page{Title: title, Body: body},nil
+}
+
+// func viewHandler(w http.ResponseWriter, r *http.Request) {
+// title:= r.URL.Path
+
+// }
 
 func main() {
-	fmt.Println("hello world")
+	p1 := &Page{Title: "test", Body: []byte("this is a sample page")}
+	p1.save()
+
+	p2,_ :=loadPage(p1.Title)
+	fmt.Println(string(p2.Body))
+	// http.HandleFunc("/view",viewHandler)
+	// log.Fatal(http.ListenAndServe(":8080",nil))
 }
